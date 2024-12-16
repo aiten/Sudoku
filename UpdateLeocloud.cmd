95afb89e0134ad3ce3156c5b71d25fe787edcf7e
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2024-12-16 23:42:29 +0100

    leocloud V3

--- UpdateLeocloud.cmd
+++ UpdateLeocloud.cmd
@@ -2,6 +2,6 @@ kubectl delete -n student-h-aitenbichler deployment sudokusolveserver
 kubectl delete -n student-h-aitenbichler service sudokusolveserver-svc
 kubectl delete -n student-h-aitenbichler ingress sudokusolveserver-ingress
 kubectl delete -n student-h-aitenbichler pod -l app=sudokusolveserver
-kubectl create -f leocloud-deploy-V2.yaml
+kubectl create -f leocloud-deploy-V3.yaml
 
 :: kubectl apply -f leocloud-deploy-V2.yaml
